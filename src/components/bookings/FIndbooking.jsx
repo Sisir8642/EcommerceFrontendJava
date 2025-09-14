fc202afa9d313c2e6828523ce0e677f19dfdd432
Baburam Bista <baburambista01@gmail.com>
2025-09-14 16:50:36 +0545

    feat: chanfed the base url

--- src/components/bookings/FIndbooking.jsx
+++ src/components/bookings/FIndbooking.jsx
@@ -9,7 +9,7 @@ const FIndbooking = () => {
     try {
       setError("");
       const response = await fetch(
-        `http://darbar-hotel.onrender.com/bookings/confirmation/${confirmationCode}`
+        `https://darbar-hotel.onrender.com/bookings/confirmation/${confirmationCode}`
       );
       if (!response.ok) throw new Error("Booking not found");
       const data = await response.json();
@@ -23,7 +23,7 @@ const FIndbooking = () => {
   const checkoutBooking = async (bookingId) => {
     try {
       await fetch(
-        `http://darbar-hotel.onrender.com/bookings/booking/${bookingId}/delete`,
+        `https://darbar-hotel.onrender.com/bookings/booking/${bookingId}/delete`,
         { method: "DELETE" }
       );
       alert("Checkout successful!");
--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -2,7 +2,7 @@ import axios from "axios"
  
 
 export const api = axios.create({
-    baseURL: "http://darbar-hotel.onrender.com/",
+    baseURL: "https://darbar-hotel.onrender.com/",
 })
 
 export async function addRoom(photo, roomType, roomPrice) {
